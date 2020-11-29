edc557002549b498d22413344acb4513b02d0bd7
nikopushkin <nikopushkin1@gmail.com>
2020-11-29 11:09:40 +0300

    new slider with styles

--- src/components/galary.vue
+++ src/components/galary.vue
@@ -1,20 +1,16 @@
 <template>
     <swiper class="swiper" :options="swiperOption">
       <swiper-slide v-for='image in photoSetSources' :key='image.id'>
-        <div>
-          <img :src="image" alt="">
-        </div>
+        <!-- <div> -->
+          <img ref='sliderImage' :src="image">
+        <!-- </div> -->
       </swiper-slide>
-      <!-- <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
-      <swiper-slide>Slide 7</swiper-slide> -->
-      <div class="swiper-pagination" slot="pagination"></div>
-      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <!-- <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
       <div class="swiper-button-next" slot="button-next"></div> -->
     </swiper>
+
+
 </template>
 
 <script>
@@ -32,8 +28,8 @@ export default {
     return {
       index: null,
       photoSetSources: [
-          "/images/photo7.jpg",
           "/images/photo1.jpg",
+          "/images/photo7.jpg",
           "/images/photo5.jpg",
           "/images/photo6.jpg",
           "/images/photo3.jpg",
@@ -46,17 +42,34 @@ export default {
         pagination: {
           el: '.swiper-pagination',
           clickable: true
+        },
+        breakpoints: {
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            slidesPerGroup: 2,
+          },
+          0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 20,
+          },
+
         }
-      }
+      },
     }
   },
 }
 </script>
 <style  scoped>
+
 .swiper {
-  height: 80vh;
-  width: 95%;
-  margin-top: 4%
+  height: 70%;
+  width: 90%;
 }
 
 .swiper-slide {
@@ -64,13 +77,41 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-  /* font-weight: bold;
-  font-size: 10 * 2; */
-  /* background-color: black; */
 }
 .swiper-slide img {
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+  padding: 0px 20px;
+  /* height: 800px; */
+  object-fit: cover !important;
+  transition: 0.4s;
 }
+
+/* @media only screen and (max-width: 1640px) {
+  .swiper-slide img {
+    width: 500px;
+    height: 500px;
+    object-fit: cover !important;
+    transition: 0.4s;
+  }
+}
+
+@media only screen and (max-width: 1400px) {
+  .swiper-slide img {
+    width: 350px;
+    height: 500px;
+    object-fit: cover !important;
+    transition: 0.4s;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .swiper-slide img {
+    width: 100%;
+    max-width: 600px;
+    height: 400px;
+    object-fit: cover !important;
+    transition: 0.4s;
+  }
+} */
 
 </style>
