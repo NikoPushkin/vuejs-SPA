75558c539855c41d081952e3bfe4fc4258f0226a
nikopushkin <nikopushkin1@gmail.com>
2021-01-18 21:21:48 +0300

    [education] fix description rendering and remove junk-styles

--- src/components/education.vue
+++ src/components/education.vue
@@ -11,7 +11,7 @@
             <span class='close-btn hover-transform-btn' @click='closeDescription(day.number)'><i class="fas fa-times"></i></span>
           </div>
 
-          <div ref='daysContent' class="days-content">{{ day.description }}</div>
+          <span ref='daysContent' class="days-content">{{ day.description }}</span>
         </div>
       </div>
     </div>
@@ -24,7 +24,8 @@ export default {
   data() {
     return {
       educationDays: {
-        1: {number: 'УСЛОВИЯ', description: '- Обучение проходит в формате online-лекции, через SKYPE.<br>- Продолжительность одной лекции: 2 - 2.5 часа <br>- Стоимость обучения - 7500 рублей.<br>*Для бронирования даты необходимо внести задаток в размере - 2500 рублей.'},
+        // 1: {number: 'УСЛОВИЯ', description: '- Обучение проходит в формате online-лекции, через SKYPE.<br>- Продолжительность одной лекции: 2 - 2.5 часа <br>- Стоимость обучения - 7500 рублей.<br>*Для бронирования даты необходимо внести задаток в размере - 2500 рублей.'},
+        1: {number: 'УСЛОВИЯ', description: 'dsadsad'},
         2: {number: 'День I', description: '- Портрет, понятие его жанров.<br>- Свет. Основы. Разбор естественного света. Световая иерархия, как работать с любым источником света?<br>- Референс как путь к собственному стилю, как грамотно составлять мудборд, вдохновение без задержек. Творческая дисциплина.'},
         3: {number: 'День II', description: '- Грамотное планирование съемки, подробный разбор нюансов подготовки к процессу.<br>- Методы поиска локаций, моделей, одежды.<br>- Модель, кто это? Основные понятия, особенности работы с профессиональными и непрофессиональными моделями. Какие ошибки вы можете допустить при работе в обеих случаях. Рабочая этика.<br>- Проблемы и конфликтные ситуации, как избежать. Влияние на психологическое состояние модели. Особенности сотрудничества с Визажистом/стилистом.<br>- Работа с моделью на съемке, установление психологического контакта Фотограф- модель. Как добиться эмоций, чувственности. Завершение съемки, почему важно получить обратную связь.'},
         4: {number: 'День III', description: '- Работа с цветом. Основные понятия, цветовые гармонии, композиция цвета.Обработка. <br>- Lightroom: Инструменты для цветокоррекции снимков, применение. Тоновая кривая, принципы работы. Основные приемы колористики, как применять их в фотографии. Экспорт фотографий. Свободные вопросы. <br>- Photoshop: Ретушь «Без ретуши» деликатный способ устранения нюансов с естественным эффектом. Коррекция формы, работа с инструментом «пластика», как и где применяю. Устранение лишних деталей с кадра, резкость, объем и блики. <br>- Портфолио Ревю. Рекомендации и свободные вопросы.'},
@@ -56,11 +57,11 @@ export default {
       // move photoset-titles to the left side when description window is opened
       this.$refs.leftPart.style.width = '50%';
 
+
       // opens fullscreen description if screen size is small
       if (window.screen.width < 950) {
-        this.$refs.leftPart.style.display = 'none'
+        this.$refs.leftPart.style.display = 'none';
       }
-
       // show description window
       this.$refs[`${descriptionRef}`][0].style.opacity = '1';
       this.$refs[`description-${descriptionRef}`][0].style.opacity = '1';
@@ -79,41 +80,6 @@ export default {
       this.$refs[`description-${descriptionRef}`][0].style.opacity = '0';
       this.$refs[`description-${descriptionRef}`][0].style.pointerEvents = 'none';
     }
-
-
-    // openDescription(refIndex, description) {
-    //   for (let ref in this.$refs) {
-    //     if (this.$refs[ref][0]) {
-    //       let refItemStyle = this.$refs[ref][0].style;
-    //       refItemStyle.opacity='0';
-    //       refItemStyle.pointerEvents = 'none'
-    //     }
-    //   }
-    //
-    //   if (window.screen.width < 950) {
-    //     this.$refs.leftPart.style.display = 'none';
-    //     // this.$refs.rightPart.style.width = '100%';
-    //   }
-    //
-    //   this.$refs.daysContainer.style.width = '100%'
-    //   this.$refs.leftPart.style.width = '50%';
-    //   this.$refs.rightPart.style.width = '50%';
-    //
-    //   this.$refs.rightPart.style.opacity = '1';
-    //   this.$refs.rightPart.style.pointerEvents = 'all';
-    //   this.$refs.daysContent.innerHTML = description;
-    // },
-    // closeDescription() {
-    //   if (window.screen.width < 950) {
-    //     this.$refs.leftPart.style.display = 'flex'
-    //   } else {
-    //     this.$refs.daysContainer.style.width = '50%'
-    //   }
-    //   this.$refs.leftPart.style.width = '100%'
-    //   this.$refs.rightPart.style.opacity = '0'
-    //   this.$refs.rightPart.style.pointerEvents = 'none'
-    //
-    // }
   }
 }
 </script>
@@ -138,139 +104,35 @@ export default {
   text-align: justify;
   color: black;
   transition: 0.2s
-  /* background: linear-gradient(to bottom, rgba(2, 2, 2, 0.2), rgba(0, 0, 0, 0.2)) */
 }
 
 .education-left-part p {
-  font-size: 25px;
+  font-size: 3vh;
   letter-spacing: 2px;
   text-transform: uppercase;
-}
-
-.course-description {
-  margin-bottom: 20px;
-}
-
-.days-container {
-  width: 50%;
-  margin: 20px 0;
-  display: flex;
-  justify-content: space-between;
-  letter-spacing: 2px
-}
-
-.days-container button{
-  background: none;
-  border: none;
-  outline: none;
-  padding: 2px 0px;
-  font-size: 20px;
-  letter-spacing: 2px;
-  transition: 0.2s;
-  will-change: transform;
-}
-
-.days-container button:hover {
-  transform: scale(1.1, 1.1);
-}
-.days-container button:focus {
-  outline: none;
-}
-
-.education-btn {
-  background: none;
-  outline: none;
-  border: none;
-  /* padding: 5px; */
-  transition: 0.2s
-}
-
-.education-btn:focus {
-  outline: none;
-  transform: scale(1.1, 1.1);
 }
 
 .education-right-part {
   position: absolute;
   width: 50%;
-  background-color:white;
-  opacity: 0;
   height: 100%;
-  transition: all 0.5s;
+  transition: 0.4s;
   right: 0 !important;
   left: auto;
+  opacity: 0;
+  background-color:white;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-content: center;
   pointer-events: none;
   color: black;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.days-description {
   text-align: justify;
-  width: 70%;
-  font-size: 18px;
 }
 
 @media screen and (max-width: 1100px) {
-  .education-left-part p {
-    font-size: 20px;
+  .education-right-part {
+    width: 100%
   }
-
-  .days-container button{
-    background: none;
-    border: none;
-    outline: none;
-    padding: 2px 0px;
-    font-size: 15px;
-    letter-spacing: 2px;
-    transition: 0.2s;
-    will-change: transform;
-  }
-}
-@media screen and (max-width: 790px) {
-  .education-left-part p {
-    font-size: 15px;
-    letter-spacing: 1px
-  }
-
-  .days-container button{
-    background: none;
-    border: none;
-    outline: none;
-    padding: 2px 0px;
-    font-size: 11px;
-    letter-spacing: 1px;
-    transition: 0.2s;
-    will-change: transform;
-  }
-}
-@media screen and (max-width: 560px) {
-  .education-left-part p {
-    font-size: 12px;
-    letter-spacing: 1px
-  }
-
-  .days-container button{
-    background: none;
-    border: none;
-    outline: none;
-    padding: 2px 0px;
-    font-size: 9px;
-    letter-spacing: 1px;
-    transition: 0.2s;
-    will-change: transform;
-  }
-
-  .days-container {
-    width: 90%
-  }
-
-  .days-description {
-    width: 80%;
-    font-size: 12px
-  }
-
 }
 </style>
