05305c8d407676e403ba910c60085eb25870bafd
nikopushkin <nikopushkin1@gmail.com>
2021-02-22 23:47:03 +0300

    [aboutMe] move swiper-btn classes to main css file

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -200,20 +200,6 @@ export default {
   font-size: 2vh
 }
 
-.swiper-button-next, .swiper-button-prev {
-  color: white !important;
-  padding: 5px !important;
-  border-radius: 50%;
-  width: calc(var(--swiper-navigation-size) / 18 * 27) !important;
-  height: calc(var(--swiper-navigation-size)*1.5) !important;
-  transition: 0.4s;
-}
-
-.swiper-button-next:hover, .swiper-button-prev:hover {
-  background-color: #e3e0e040;
-  transform: scale(1.1, 1.1);
-}
-
 /*
 
 * DESKTOP ABOUTME PAGE STYLES
